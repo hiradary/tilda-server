6a4c584aa48d6f61025b95084d3cfdaf0dd6643d
Hirad Arshadi <hiradwork@gmail.com>
2021-12-19 00:43:40 +0330

    Testing husky

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ const app = express()
 const port = 3000
 
 const start = () => {
-  var sag = 'salam'
   app.listen(port, () => {
     console.log(`Tilda application is running on port ${port}.`)
   })
